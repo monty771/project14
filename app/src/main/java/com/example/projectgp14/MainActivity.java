f0904087d3d0d588d16619053a7cb53fcd07caaa
Mont24 <127741528+Mont24@users.noreply.github.com>
2023-12-07 14:14:02 +0000

    home page

--- app/src/main/java/com/example/projectgp14/MainActivity.java
+++ app/src/main/java/com/example/projectgp14/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.projectgp14;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button login = (Button) findViewById(R.id.btn_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent lO = new Intent(MainActivity.this,);
+                startActivity(lO);
+            }
+        });
+
+        Button signup = (Button) findViewById(R.id.btn_signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sU = new Intent(MainActivity.this,);
+                startActivity(sU);
+            }
+        });
+
+        Button solar = (Button) findViewById(R.id.btn_solarpan);
+        solar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent s = new Intent(MainActivity.this,);
+                startActivity(s);
+            }
+        });
     }
 }
